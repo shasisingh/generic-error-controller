1b6fa6a0f5b4fddfc99f7c20d4b41f63a72904f5
Shasi Singh <shasi.singh@rabobank.nl>
2024-08-15 21:34:56 +0200

    added some unittest

---
+++ src/test/java/nl/nightcrawler/spring/webcontroller/controller/ErrorControllerTest.java
@@ -0,0 +1,49 @@
+package nl.nightcrawler.spring.webcontroller.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class ErrorControllerTest {
+
+    @Test
+    void testCheckCustomErrorController() {
+
+        ErrorAttributes errorAttributes = new DefaultErrorAttributes();
+        ErrorController errorController = new ErrorController(errorAttributes);
+        HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+        when(httpServletRequest.getAttribute("jakarta.servlet.error.status_code")).thenReturn(403);
+        when(httpServletRequest.getAttribute("jakarta.servlet.error.exception")).thenReturn(new RuntimeException("No such method"));
+        ResponseEntity<ErrorController.ErrorResponse> response = errorController.errorPost(httpServletRequest);
+        assertEquals(403, response.getStatusCode().value());
+        assertNotNull(response.getBody().getMessage());
+        assertNotNull(response.getBody().getTimestamp());
+        assertNotEquals(0, response.getBody().getStatus());
+        assertEquals("Forbidden", response.getBody().getError());
+    }
+
+    @Test
+    void HttpMediaTypeNotSupportedException() {
+
+        ErrorAttributes errorAttributes = new DefaultErrorAttributes();
+        ErrorController errorController = new ErrorController(errorAttributes);
+        HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+        when(httpServletRequest.getAttribute("jakarta.servlet.error.status_code")).thenReturn(415);
+        when(httpServletRequest.getAttribute("jakarta.servlet.error.exception")).thenReturn(new HttpMediaTypeNotSupportedException("media type is not supported"));
+        ResponseEntity<ErrorController.ErrorResponse> response = errorController.errorGet(httpServletRequest);
+        assertEquals(HttpStatus.UNSUPPORTED_MEDIA_TYPE, response.getStatusCode());
+        assertEquals(415, response.getBody().getStatus());
+        assertEquals("media type is not supported", response.getBody().getMessage());
+    }
+
+
+}
