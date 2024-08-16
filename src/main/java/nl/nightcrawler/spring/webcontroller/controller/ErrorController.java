64013fdc18f67f50af4587d7548bb97dfa9e40ab
Shasi Singh <shasi.singh@rabobank.nl>
2024-08-16 20:45:05 +0200

    added some unittest

--- src/main/java/nl/nightcrawler/spring/webcontroller/controller/ErrorController.java
+++ src/main/java/nl/nightcrawler/spring/webcontroller/controller/ErrorController.java
@@ -10,13 +10,12 @@ import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("${server.error.path:${error.path:/error}}")
@@ -26,31 +25,33 @@ public class ErrorController extends AbstractErrorController {
         super(errorAttributes);
     }
 
-    @GetMapping
-    public ResponseEntity<ErrorResponse> errorGet(HttpServletRequest request) {
+    @RequestMapping
+    public ResponseEntity<ErrorResponse> errorJson(HttpServletRequest request) {
         return getErrorResponseResponseEntity(request);
     }
 
-    @PostMapping
-    public ResponseEntity<ErrorResponse> errorPost(HttpServletRequest request) {
-        return getErrorResponseResponseEntity(request);
-    }
     private ResponseEntity<ErrorResponse> getErrorResponseResponseEntity(HttpServletRequest request) {
         var errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.of(getALLErrorAttribute()));
         var httpStatus = getStatus(request);
-        var errorResponse = getErrorResponse(getMessage(errorAttributes), httpStatus);
+        var errorResponse = getErrorResponse(errorAttributes, httpStatus);
         return ResponseEntity.status(httpStatus).body(errorResponse);
     }
+
     private static String getMessage(Map<String, Object> errorAttributes) {
-        return errorAttributes != null ? errorAttributes.getOrDefault("message", "").toString() : "";
+        return Objects.nonNull(errorAttributes) ? errorAttributes.getOrDefault("message", "").toString() : "";
     }
 
-    private static ErrorResponse getErrorResponse(final String message, final HttpStatus httpStatus) {
+    private static String getPath(Map<String, Object> errorAttributes) {
+        return Objects.nonNull(errorAttributes) ? errorAttributes.getOrDefault("path", "").toString() : "";
+    }
+
+    private static ErrorResponse getErrorResponse(Map<String, Object> errorAttributes, final HttpStatus httpStatus) {
         return ErrorResponse.builder()
                 .timestamp(ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
                 .status(httpStatus.value())
+                .path(getPath(errorAttributes))
+                .message(getMessage(errorAttributes))
                 .error(httpStatus.getReasonPhrase())
-                .message(message)
                 .build();
     }
 
@@ -58,10 +59,11 @@ public class ErrorController extends AbstractErrorController {
     @EqualsAndHashCode
     @Builder
     public static final class ErrorResponse {
-        private final String timestamp;
         private final int status;
+        private final String timestamp;
         private final String error;
         private final String message;
+        private final String path;
 
     }
 
